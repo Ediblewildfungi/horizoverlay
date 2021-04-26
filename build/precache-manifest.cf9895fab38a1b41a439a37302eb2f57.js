1416b16d4d687654d47c2e696d6811422fc08c7e
Ediblewildfungi <jianghhui@gmail.com>
2021-04-26 22:28:41 +0800

    update

---
+++ build/precache-manifest.cf9895fab38a1b41a439a37302eb2f57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c998d22a3dfe896a24511ac53354f5",
+    "url": "/horizoverlay/index.html"
+  },
+  {
+    "revision": "e60045e7c7e87ad7ccd6",
+    "url": "/horizoverlay/static/css/main.19ee216d.chunk.css"
+  },
+  {
+    "revision": "92e0281c4cc45a56033e",
+    "url": "/horizoverlay/static/js/2.fc160fe0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/horizoverlay/static/js/2.fc160fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e60045e7c7e87ad7ccd6",
+    "url": "/horizoverlay/static/js/main.16c96671.chunk.js"
+  },
+  {
+    "revision": "30f65308d7bdcba22167",
+    "url": "/horizoverlay/static/js/runtime-main.9a241b58.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "/horizoverlay/static/media/mog.e1e18820.png"
+  }
+]);
